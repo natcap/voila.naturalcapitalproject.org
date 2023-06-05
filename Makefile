01326d0e22f8bd6b9e400ae40dadf0736548dcbf
James Douglass <jdouglass@stanford.edu>
2023-06-05 15:27:25 -0700

    Laying groundwork for a notebook.

---
+++ Makefile
@@ -0,0 +1,29 @@
+INTER := intermediate
+ENV := env
+
+$(ENV):
+	mamba create -p $(ENV) -y -c conda-forge pygeoprocessing voila jupyter wget
+
+$(INTER):
+	mkdir intermediate
+
+$(INTER)/Base_Data.zip: $(INTER) $(ENV)
+	$(ENV)/bin/wget -O $(INTER)/Base_Data.zip https://storage.googleapis.com/releases.naturalcapitalproject.org/invest/3.13.0/data/Base_Data.zip
+
+$(INTER)/global_dem.tif: $(INTER)/Base_Data.zip
+	unzip -p $(INTER)/Base_Data.zip Base_Data/global_dem.tif > $(INTER)/global_dem.tif
+
+$(INTER)/rescaled.tif: $(INTER)/global_dem.tif
+	$(ENV)/bin/python rescale.py $(INTER)/global_dem.tif $(INTER)/rescaled.tif
+
+$(INTER)/elevation.tif: $(INTER)/rescaled.tif
+	$(ENV)/bin/gdaldem color-relief -co COMPRESS=LZW $(INTER)/rescaled.tif elevation-color-profile-grass.txt $(INTER)/elevation.tif
+
+tiles: $(INTER)/elevation.tif
+	$(ENV)/bin/gdal2tiles.py --xyz -r near --zoom=1-7 --processes=5 $(INTER)/elevation.tif tiles
+
+serve-jupyter: tiles
+	jupyter notebook demo.ipynb
+
+serve-voila: tiles
+	voila demo.ipynb
---
+++ rescale.py
@@ -0,0 +1,40 @@
+import logging
+import sys
+
+logging.basicConfig(level=logging.INFO)
+
+import numpy
+import pygeoprocessing
+from osgeo import gdal
+
+source_raster = sys.argv[1]
+target_raster = sys.argv[2]
+
+nodata = pygeoprocessing.get_raster_info(source_raster)['nodata'][0]
+
+minimum = float('inf')
+maximum = float('-inf')
+
+for data, block in pygeoprocessing.iterblocks((source_raster, 1)):
+    if nodata is not None:
+        valid_pixels = ~numpy.isclose(block, nodata)
+    else:
+        valid_pixels = numpy.ones(block.shape, dtype=bool)
+
+    minimum = min(block[valid_pixels].min(), minimum)
+    maximum = max(block[valid_pixels].max(), maximum)
+
+def rescale(block):
+    if nodata is not None:
+        valid_pixels = ~numpy.isclose(block, nodata)
+    else:
+        valid_pixels = True
+    target = numpy.full(block.shape, -1, dtype=numpy.uint8)
+
+    target[valid_pixels] = numpy.interp(block[valid_pixels], (minimum, maximum), (0, 254))
+
+    return target
+
+pygeoprocessing.raster_calculator(
+    [(source_raster, 1)], rescale, target_raster, gdal.GDT_Int16, -1
+)
