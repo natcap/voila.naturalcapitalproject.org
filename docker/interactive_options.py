edb53ad00ecc046eea1d16215eee54814374f71e
James Douglass <jdouglass@stanford.edu>
2023-08-31 20:53:48 -0700

    Noting another gdal config option suggested by the docs.

--- docker/interactive_options.py
+++ docker/interactive_options.py
@@ -33,6 +33,7 @@ def compute(source_raster_path, aoi_geom, target_epsg, target_raster_path,
     from osgeo import osr
 
     gdal.SetConfigOption('CPL_VSIL_USE_TEMP_FILE_FOR_RANDOM_WRITE', 'YES')
+    gdal.SetConfigOption('CPL_MACHINE_IS_GCE', 'YES')
     WGS84_SRS = osr.SpatialReference()
     WGS84_SRS.ImportFromEPSG(4326)
     gdal.DontUseExceptions()
