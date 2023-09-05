56bf3d08c10a606d67b8b0b9ebec4c5ed1bbb6bf
James Douglass <jdouglass@stanford.edu>
2023-09-05 16:50:01 -0700

    Attempting to mount our GCS bucket via FUSE into the container.

---
+++ docker/gcsfuse_run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# [START cloudrun_fuse_script]
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Create mount directory for service
+mkdir -p "$MNT_DIR"
+
+echo "Mounting GCS Fuse."
+gcsfuse --debug_gcs --debug_fuse "$BUCKET" "$MNT_DIR"
+echo "Mounting completed."
+
+exec /opt/conda/bin/voila --Voila-ip=0.0.0.0 --no-browser --show-tracebacks=True /opt/app.ipynb
+
+# [END cloudrun_fuse_script]
