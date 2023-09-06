73d7ffe5fcd36c52064f418a7cce06e07e27fc12
James Douglass <jdouglass@stanford.edu>
2023-09-05 17:34:45 -0700

    Changing how the mount point is created.

--- docker/gcsfuse_run.sh
+++ docker/gcsfuse_run.sh
@@ -16,9 +16,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# Create mount directory for service
-mkdir -p "$MNT_DIR"
-
 echo "Mounting GCS Fuse."
 gcsfuse --debug_gcs --debug_fuse "$BUCKET" "$MNT_DIR"
 echo "Mounting completed."
