6e420b5a4d2d3460386ff133c9bf25af7e018247
James Douglass <jdouglass@stanford.edu>
2023-09-06 21:14:23 -0700

    Returning to exec.

--- docker/gcsfuse_run.sh
+++ docker/gcsfuse_run.sh
@@ -21,13 +21,13 @@ gcsfuse --debug_gcs --debug_fuse "$BUCKET" "$MNT_DIR"
 echo "Mounting completed."
 
 echo "Starting voila"
-/opt/conda/bin/voila \
+exec /opt/conda/bin/voila \
     --debug \
     --no-browser \
     --port "$PORT" \
     --pool_size 2 \
     --preheat_kernel True \
-    --show_tracebacks True /opt/app.ipynb &
+    --show_tracebacks True /opt/app.ipynb
 echo "Voila started."
 
 # [END cloudrun_fuse_script]
